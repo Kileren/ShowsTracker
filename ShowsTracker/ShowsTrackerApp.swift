d1446e5a0ff44ed5de16b99fafcde4b5121a064e
s.bogachev <s.bogachev@tinkoff.ru>
2021-01-28 20:12:54 +0300

    Initial commit

---
+++ ShowsTracker/ShowsTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowsTrackerApp.swift
+//  ShowsTracker
+//
+//  Created by s.bogachev on 28.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct ShowsTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
